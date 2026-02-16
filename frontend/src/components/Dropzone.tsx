46b38eee2dd6ddb296a45e78139c068bf13bd066
Richard Li <rgli@uwaterloo.ca>
2026-02-15 20:56:35 -0500

    Created dropzone component

--- frontend/src/components/Dropzone.tsx
+++ frontend/src/components/Dropzone.tsx
@@ -1,10 +1,13 @@
 export default function Dropzone() {
+
+    const handleClick
+
     return (
         <>
-            <div id="drop-zone">
+            <div id="drop-zone" onClick={handleClick}>
                 <p>Upload file from computer or drag and drop file</p>
-                <input type="file" name="upload-file" accept=".pdf"/>
             </div>
+            <input id="upload" type="file" accept=".pdf"/>
         </>
     )
 }
