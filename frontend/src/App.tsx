bb4db713874b57c8fdd1497dfb70fcedfb91a5d7
Richard Li <rgli@uwaterloo.ca>
2026-05-01 22:03:19 -0400

    Added login button and formatted text

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
 import Dropzone from "./components/Dropzone";
+import Searchbar from "./components/Searchbar";
 
 function App() {
   return (
     <>
-      <h1>docsearcher</h1>
-      <Dropzone />
+      <button id="login-button" type="button">Login</button>
+      <div className="background">
+        <div>
+          <p id="title">docsearcher</p>
+          <p id="subtitle">
+            a goated ai tool for finding specific information in PDFs WITHOUT keywords
+          </p>
+          <Searchbar />
+          <p id="description">
+            Describe what this website is and explain why its a better alternative to regular searching using ctrl + f as;dlkf jas;lf jawra; igheoaiugh o;asfkh ao;irguh ogiu ha;s oigha;sofgia;sofgi has;ofgi ha;oguh ear;ogi ha;goisadh o;osif gha;ogi hraw;og ihro;aihas;oflkjas f;oewoi;rigiroirwofgh aw or fiho idoshfasd fhasd;k
+          </p>
+        </div>
+        <Dropzone />
+      </div>
     </>
   )
 }
---
+++ frontend/src/components/Searchbar.tsx
@@ -0,0 +1,12 @@
+import { Search } from "lucide-react";
+
+export default function Searchbar() {
+    return (
+        <>
+            <div id="search-bar">
+                <Search />
+                <p>Search</p>
+            </div>
+        </>
+    )
+}
